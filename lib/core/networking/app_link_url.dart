9914811e5616398c3e4cb152897bd2bad906acd2
georgeAlNa <georgenasser2002@gmail.com>
2026-04-25 19:01:00 -0700

    Update base URL

--- lib/core/networking/app_link_url.dart
+++ lib/core/networking/app_link_url.dart
@@ -1,5 +1,5 @@
 class AppLinkUrl {
-  static const String baseUrl = "http://localhost:8000/api/v1";
+  static const String baseUrl = "https://api.35.228.22.79.nip.io/api/v1";
   static const String login = "/auth/login/";
   static const String register = "/auth/register/";
   static const String logout = "/auth/logout/";
